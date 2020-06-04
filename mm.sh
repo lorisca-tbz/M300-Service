581086e4c93992a84c35960f9119ddd541b300e7
lorisca-tbz <loris.cagnazzo@lernende.ethz.ch>
2020-06-04 09:18:48 +0200

    mm.sh zweite Version

--- mm.sh
+++ mm.sh
@@ -6,7 +6,7 @@
 #	Verwendetete Ports wie folgt abfragen:
 #	vagrant port
 
-for vm in web01 web02 web03 web04
+for vm in web01 web02
 do
 
     mkdir ${vm}
@@ -32,11 +32,31 @@ do
     cat <<%EOF% >index.html
     <html>
         <body>
-            <h1>Hallo ${vm}</h1>
+            <h1>Loris Cagnazzo's LB2 ${vm}</h1>
         </body>
     <html>
 %EOF%
     vagrant up
     cd ..
     
+for vm in db01 db02
+do
+
+    mkdir ${vm}
+    cd ${vm}
+    
+    # Vagrantfile 
+    cat <<%EOF% >Vagrantfile
+        Vagrant.configure(2) do |config|
+          config.vm.box = "ubuntu/xenial64"
+          config.vm.network "forwarded_port", guest:80, host:8080, auto_correct: true
+          config.vm.synced_folder ".", "/var/www/html"  
+        config.vm.provider "virtualbox" do |vb|
+          vb.memory = "256"  
+        end
+        config.vm.provision "shell", inline: <<-SHELL 
+          sudo apt-get update
+          sudo apt-get -y install apache2
+        SHELL
+        end
 done
